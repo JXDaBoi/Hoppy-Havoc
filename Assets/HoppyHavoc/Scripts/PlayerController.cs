435087d3843a1b460499eae3ff89a7a88b749dcd
JasonYap2000 <marinej2020@gmail.com>
2024-10-19 01:39:46 +0800

    Interactive Grass

--- Assets/HoppyHavoc/Scripts/PlayerController.cs
+++ Assets/HoppyHavoc/Scripts/PlayerController.cs
@@ -43,7 +43,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-       
+        Shader.SetGlobalVector("_Player", transform.position);
     }
 
     private void HandleCameraRotation()
